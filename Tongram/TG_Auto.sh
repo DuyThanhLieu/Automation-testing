ddeff40f2e6151548c363a9fc928f829673ea573
DuyThanhLieu <duythanhlieu2001@gmail.com>
2024-12-27 11:46:59 +0700

    cicd

--- Tongram/TG_Auto.sh
+++ Tongram/TG_Auto.sh
@@ -5,8 +5,8 @@
 # npx playwright test TG_VerifyPoint.spec.js --reporter=html,json --output=./Results --workers=1
 
 # npx playwright test TG_Actions.spec.js --reporter=line --workers=1
-# npx playwright test TG_Languages.spec.js --reporter=line --workers=1
+npx playwright test TG_Languages.spec.js --reporter=line --workers=1 --headed
 
 # npx playwright test TG_DailyTask.spec.js TG_VerifyPoint.spec.js TG_Actions.spec.js TG_Languages.spec.js --headed --reporter=html --output=./Results --workers=1
 
-npx playwright test TG_DailyTask.spec.js TG_VerifyPoint.spec.js TG_Actions.spec.js TG_Languages.spec.js --reporter=html --output=./Results --workers=1
+# npx playwright test TG_DailyTask.spec.js TG_VerifyPoint.spec.js TG_Actions.spec.js TG_Languages.spec.js --reporter=html --output=./Results --workers=1
